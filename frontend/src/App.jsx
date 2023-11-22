ee61582ab4f8d335cf76cb677fa9fda58b330260
Avinash Kumar <avinashkumar532004@gmail.com>
2023-11-22 09:07:39 +0530

    deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,11 @@ import Header from "./components/Header";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Dashboard from "./components/Dashboard";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createHashRouter,
+} from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import Homepage from "./pages/Homepage";
 import { fetchUser } from "./features/middleware/userMiddleware";
@@ -11,7 +15,7 @@ import { fetchToDo } from "./features/middleware/toDoMiddleware";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Homepage />,
